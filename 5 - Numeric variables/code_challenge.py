39198c52333e844f785928e2cd6f4ad1923cc23f
Bednarczyk Filip <filip.bednarczyk@immalle.eu>
2019-10-11 09:11:36 +0200

    oef 2-5

--- 5 - Numeric variables/code_challenge.py
+++ 5 - Numeric variables/code_challenge.py
@@ -1,6 +1,13 @@
 # Ask a user to enter a number
+number = input('Please enter a number. ')
+
 # Ask a user to enter a second number
+number2 = input('Please enter a second number. ')
+
 # Calculate the total of the two numbers added together
+answer = int(number) + int(number2)
+
 # Print 'first number + second number = answer' 
 # For example if someone enters 4 and 6 the output should read
 # 4 + 6 = 10
+print(number + ' + ' + number2 + " = " + str(answer))
